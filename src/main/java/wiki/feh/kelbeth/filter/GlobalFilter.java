14ca752881c46b35876c547300afc57d4bf2d2b4
Cass07 <kjh95828@naver.com>
2026-07-23 20:21:34 +0900

    [Feature] GlobalFilter를 통해 RequestID 부여 기능 추가
    - 로깅용 Request ID 부여 기능 추가함

--- src/main/java/wiki/feh/kelbeth/filter/GlobalFilter.java
+++ src/main/java/wiki/feh/kelbeth/filter/GlobalFilter.java
@@ -20,8 +20,7 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 		super(Config.class);
 	}
 
-	public static class Config {
-	}
+	private static final String HEADER_REQUEST_ID = "X-Request-ID";
 
 	@Override
 	public org.springframework.cloud.gateway.filter.GatewayFilter apply(Config config) {
@@ -32,21 +31,41 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 			// 요청 처리 전 로직
 			StopWatch stopWatch = new StopWatch();
 			stopWatch.start();
-			log.info("[GlobalFilter] Request Path: {}", request.getPath());
 
-			return chain.filter(exchange)
+			// requestId 생성, 존재하면 그대로 사용
+			String requestIdHeader = request.getHeaders().getFirst(HEADER_REQUEST_ID);
+			String requestId = (requestIdHeader != null && !requestIdHeader.isEmpty()) ? requestIdHeader :
+				java.util.UUID.randomUUID().toString();
+
+			log.info("[GlobalFilter] ID: {} Request Path: {}", requestId, request.getPath());
+
+			// header에 requestId 추가
+			var mutatedRequest = request.mutate()
+				.header(HEADER_REQUEST_ID, requestId)
+				.build();
+
+			var mutatedExchange = exchange.mutate()
+				.request(mutatedRequest)
+				.build();
+
+			return chain.filter(mutatedExchange)
 				.onErrorResume(throwable -> {
-					// 예외 처리 로직
-					log.error("[GlobalFilter] Exception occurred: {}", throwable.getMessage());
+					// 필터에서 예상 못 한 예외 발생했을때 처리 로직
+					log.error("[GlobalFilter] ID: {} Exception occurred: {}", requestId, throwable.getMessage());
 					response.setStatusCode(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
 					return response.setComplete();
 				})
 				.doFinally(_ -> {
 					// 응답 처리 후 로직
 					stopWatch.stop();
-					log.info("[GlobalFilter] Response Status Code: {}", response.getStatusCode());
-					log.info("[GlobalFilter] Request processing time: {} ms", stopWatch.getTotalTimeMillis());
+					log.info("[GlobalFilter] ID: {} Response Status Code: {}", requestId, response.getStatusCode());
+					log.info("[GlobalFilter] ID: {} Request processing time: {} ms", requestId,
+						stopWatch.getTotalTimeMillis());
+					exchange.getResponse().getHeaders().add(HEADER_REQUEST_ID, requestId);
 				});
 		};
+	}
+
+	public static class Config {
 	}
 }
